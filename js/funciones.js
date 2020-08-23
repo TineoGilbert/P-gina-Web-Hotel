526199636fbaa2a6a7b24694b7badc7570408177
Gilbert Tineo <tineogilbert@gmail.com>
2020-08-23 17:41:08 -0400

    Página Web Hotel

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+/*Acción para llamar a los Inputs del Formulario (Área de reservación)*/
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
+        
+ 
